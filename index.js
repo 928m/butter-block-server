896daa77818bee8054faa93a53495118ba72d19c
Min jihee <jihee9289@gmail.com>
2019-03-21 14:15:11 +0900

    send and receive 3D data & user counting logic modification

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const io = require('socket.io')(http);
 const problems = ['개미', '계란', '사과', '나무', 'BTS', '똥', 'ken', 'javascript'];
 const userIds = [];
 const userNickNames = [];
+const cubes = [];
+let userCount = 0;
 let problemCount = 0;
 
 io.on('connection', function (socket) {
   const userId = socket.client.id;
 
   socket.on('user', function (nickname) {
-    if (userIds.length <= 3) {
+    userCount++;
+
+    if (userCount <= 3) {
       userIds.push(userId);
       userNickNames.push(nickname);
 
@@ -34,16 +38,26 @@ io.on('connection', function (socket) {
         io.to(userIds[problemCount]).emit('submission', problems[problemCount]);
       }
     } else {
+      userCount--;
       socket.emit('order', 'be full');
     }
+  });
+
+  socket.on('create cube', function (cube) {
+    cubes.push(cube);
+
+    socket.broadcast.emit('cubes', cubes);
   });
 
   socket.on('disconnect', function (reason) {
     if (reason === 'transport close') {
       const removeIndex = userIds.indexOf(userId);
 
+      userCount--;
       userIds.splice(removeIndex, 1);
       userNickNames.splice(removeIndex, 1);
+
+      io.emit('users', userNickNames);
     }
   });
 });
